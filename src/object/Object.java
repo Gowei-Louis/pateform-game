95918898f896ee28e0006d33c49d67bc2839b3f6
Arthur Laisney <77444753+GOWEY@users.noreply.github.com>
2023-05-30 16:56:47 +0200

    javadoc

--- src/object/Object.java
+++ src/object/Object.java
@@ -5,34 +5,76 @@ import main.GamePanel;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+/**
+ * Cette classe représente un objet du jeu.
+ */
 public class Object {
 
+    /**
+     * La position X de l'objet dans le monde.
+     */
+    public int worldX;
 
-    public int worldX, worldY;
+    /**
+     * La position Y de l'objet dans le monde.
+     */
+    public int worldY;
+
+    /**
+     * L'image de l'objet.
+     */
     public BufferedImage image;
 
+    /**
+     * Le nom de l'objet.
+     */
     public String name;
+
+    /**
+     * Indique si l'objet est en collision avec d'autres objets.
+     */
     public boolean collision = false;
-    public Rectangle solidArea = new Rectangle(0,0, 48,48);
+
+    /**
+     * La zone solide de l'objet représentée par un rectangle.
+     * Par défaut, la taille du rectangle est de 48x48.
+     */
+    public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+
+    /**
+     * La position X par défaut de la zone solide de l'objet.
+     */
     public int solidAreaDefaultX = 0;
+
+    /**
+     * La position Y par défaut de la zone solide de l'objet.
+     */
     public int solidAreaDefaultY = 0;
 
-
+    /**
+     * Constructeur de la classe Object.
+     *
+     * @param worldX La position X de l'objet dans le monde.
+     * @param worldY La position Y de l'objet dans le monde.
+     */
     public Object(int worldX, int worldY) {
         this.worldX = worldX;
         this.worldY = worldY;
     }
 
+    /**
+     * Dessine l'objet sur le panneau de jeu.
+     *
+     * @param g2d        Le contexte graphique 2D.
+     * @param gamePanel  Le panneau de jeu.
+     */
     public void draw(Graphics2D g2d, GamePanel gamePanel) {
-        int screenX = worldX - gamePanel.player.worldX + gamePanel.screenWidth/2;
-        int screenY = worldY - gamePanel.player.worldY + gamePanel.screenHeight/2;
+        int screenX = worldX - gamePanel.player.worldX + gamePanel.screenWidth / 2;
+        int screenY = worldY - gamePanel.player.worldY + gamePanel.screenHeight / 2;
 
-        if(worldX + gamePanel.tileSize > gamePanel.player.worldX - gamePanel.screenWidth/2 && worldX  - gamePanel.tileSize < gamePanel.player.worldX + gamePanel.screenWidth/2 &&
-                worldY  + gamePanel.tileSize > gamePanel.player.worldY - gamePanel.screenHeight/2 && worldY  - gamePanel.tileSize  < gamePanel.player.worldY + gamePanel.screenHeight/2){
+        if (worldX + gamePanel.tileSize > gamePanel.player.worldX - gamePanel.screenWidth / 2 && worldX - gamePanel.tileSize < gamePanel.player.worldX + gamePanel.screenWidth / 2 &&
+                worldY + gamePanel.tileSize > gamePanel.player.worldY - gamePanel.screenHeight / 2 && worldY - gamePanel.tileSize < gamePanel.player.worldY + gamePanel.screenHeight / 2) {
             g2d.drawImage(image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
         }
-
-
-
     }
 }
--- src/object/ObjectBoots.java
+++ src/object/ObjectBoots.java
@@ -2,14 +2,28 @@ package object;
 
 import java.io.InputStream;
 
-public class ObjectBoots extends Object{
+/**
+ * Cette classe représente un objet de type "ObjectBoots" (objet de bottes) dans le jeu.
+ * Il s'agit d'un sous-classe de la classe "Object".
+ */
+public class ObjectBoots extends Object {
 
+    /**
+     * Constructeur de la classe ObjectBoots.
+     *
+     * @param worldX La position X de l'objet dans le monde.
+     * @param worldY La position Y de l'objet dans le monde.
+     */
     public ObjectBoots(int worldX, int worldY) {
         super(worldX, worldY);
+
+        // Lecture de l'image de l'objet à partir du fichier boots.png
         InputStream inputStream = getClass().getResourceAsStream("/object_data/boots.png");
         if (inputStream == null) {
             throw new NullPointerException("Le fichier boots.png est introuvable dans le dossier object_data.");
         }
+
+        // Affectation du nom de l'objet
         name = "boots";
     }
 
